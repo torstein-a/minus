160d05b2fdc74fc03bfcecb8e50db70b89b89e87
torstein altin <torstein@boost.no>
2015-06-16 09:41:04 +0200

    Better colours, moar blocks

--- opera-presto/minus.js
+++ opera-presto/minus.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @include http://*.adressa.no/*
+// @include http://*.t-a.no/*
 // @include http://*.vg.no/*
 // @include http://*.dagbladet.no/*
 // @include http://*.dn.no/*
@@ -12,20 +13,50 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-if (location.hostname.match("adressa.no")) {
+var blandColour = function() {
+    return "rgba(" + ((60 + Math.random() * 80)|0) +
+        "," + ((60 + Math.random() * 80)|0) +
+        "," + ((60 + Math.random() * 80)|0) +
+        ",.3)";
+};
+
+// Polaris
+if (location.hostname.match(/((adressa)|(t-a))\.no/)) {
     document.addEventListener('DOMContentLoaded', function () {
-        Array.prototype.forEach.call(document.querySelectorAll('.payed'), function (el) {
+        Array.prototype.forEach.call(document.querySelectorAll('.payed > *'), function (el) {
             el.style.visibility = "hidden";
-            el.parentNode.style.background = "rgba(99,66,66,.3)";
+            el.parentNode.style.background = blandColour();
         });
     });
 }
 
 if (location.hostname.match("vg.no")) {
     document.addEventListener('DOMContentLoaded', function () {
-        Array.prototype.forEach.call(document.querySelectorAll('a[href^="http://pluss.vg"]'), function (el) {
+        // Plus and VGtv
+        Array.prototype.forEach.call(document.querySelectorAll('a[href^="http://pluss.vg"], .df-img-skin-play-button, .video-unit, .vgpluss-header'), function (el) {
+            console.log(el);
             el.parentNode.style.visibility = "hidden";
-            el.parentNode.parentNode.style.background = "rgba(99,66,66,.3)";
+            el.parentNode.parentNode.style.background = blandColour();
+        });
+    });
+}
+
+if (location.hostname.match("dagbladet.no")) {
+    document.addEventListener('DOMContentLoaded', function () {
+        // Plus
+        Array.prototype.forEach.call(document.querySelectorAll('.plussArticle, .plussbox_article'), function (el) {
+            el.style.visibility = "hidden";
+            el.parentNode.style.background = blandColour();
+        });
+        // DBtv
+        Array.prototype.forEach.call(document.querySelectorAll('a[href^="http://dbtv.no"]'), function (el) {
+            el.parentNode.style.visibility = "hidden";
+            el.parentNode.parentNode.style.background = blandColour();
+        });
+        //Se.no
+        Array.prototype.forEach.call(document.querySelectorAll('.a[href^="http://se.no"]'), function (el) {
+            el.style.visibility = "hidden";
+            el.parentNode.style.background = blandColour();
         });
     });
 }
@@ -33,8 +64,10 @@ if (location.hostname.match("vg.no")) {
 if (location.hostname.match("dn.no")) {
     document.addEventListener('DOMContentLoaded', function () {
         Array.prototype.forEach.call(document.querySelectorAll('.df-skin-paid > div'), function (el) {
+            console.log(el);
             el.style.visibility = "hidden";
-            el.parentNode.style.background = "rgba(99,66,66,.3)";
+            el.parentNode.style.background = blandColour();
         });
     });
 }
+
